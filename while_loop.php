4fe522ea2bd2839ca54c02b7670a9f95e3a3099c
Bayzid Simon Sarkar <80593954+BayzidCTG428@users.noreply.github.com>
2025-12-24 01:22:01 +0600

    Add files via upload

---
+++ while_loop.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>While Loop</title>
+</head>
+<body>
+    <?php
+        $counter = 0;   
+
+        while ($counter < 10) {
+            echo "Hello World"."<br>";
+            $counter++;
+
+        $counternum = 0;   
+        }
+
+        while ($counternum <= 10) {
+            echo $counternum."<br>";
+            $counternum++;
+        }
+    ?>
+</body>
+</html>
