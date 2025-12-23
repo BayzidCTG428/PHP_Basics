b4a362b9c5e443b13f55fcace74f98c6d6a17182
Bayzid Simon Sarkar <80593954+BayzidCTG428@users.noreply.github.com>
2025-12-24 01:22:40 +0600

    Update array.php

--- array.php
+++ array.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Array</title>
 </head>
 <body>
     <?php 
@@ -32,4 +32,5 @@
 
     ?>
 </body>
+
 </html>
