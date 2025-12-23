8effb67ba1a15b36b35ba81ea74f5e1ada8bf898
Bayzid Simon Sarkar <80593954+BayzidCTG428@users.noreply.github.com>
2025-12-24 01:23:36 +0600

    Update associative_array.php

--- associative_array.php
+++ associative_array.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Associated Array</title>
+    <title>Associative Array</title>
 </head>
 <body>
     <?php
@@ -13,3 +13,4 @@
 </body>
 
 </html>
+
